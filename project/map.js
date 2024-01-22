1e6b4e5b7ca227d026888933baf877ebfa148a30
dragon0622 <iamdragon.dev@gmail.com>
2024-01-22 16:20:18 +0900

    Feat : split map js file

---
+++ project/map.js
@@ -0,0 +1,12 @@
+var mapContainer = document.getElementById('map'), // 지도를 표시할 div
+    mapOption = {
+        center: new kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
+        level: 3 // 지도의 확대 레벨
+    };
+
+// 지도를 표시할 div와  지도 옵션으로  지도를 생성합니다
+let map = new kakao.maps.Map(mapContainer, mapOption);
+var mapTypeControl = new kakao.maps.MapTypeControl();
+map.addControl(mapTypeControl, kakao.maps.ControlPosition.TOPRIGHT);
+var zoomControl = new kakao.maps.ZoomControl();
+map.addControl(zoomControl, kakao.maps.ControlPosition.RIGHT);
